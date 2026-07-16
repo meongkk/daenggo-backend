1a3e755358edfa77614ba4bb1c01ecd18ac339fe
alsthdua107 <alsthdua107@gmail.com>
2026-07-16 13:38:08 +0900

    feat: 산책 참여 반려동물 Entity 추가

---
+++ src/main/java/com/daenggo/backend/walk/entity/WalkRecordPet.java
@@ -0,0 +1,37 @@
+package com.daenggo.backend.walk.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "walk_record_pets")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class WalkRecordPet {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="walk_record_pet_id")
+	private Long walkRecordPetId;
+	
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name="pet_id", nullable = false)
+//	private Pet pet; 						  					// 아직 entity가 없어 오류 발생 -> 주석 처리
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="walk_record_id", nullable = false)
+	private WalkRecord walkRecord;
+}

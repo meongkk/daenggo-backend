baff653798b3e4508794ba6e67a5c01866aafa34
alsthdua107 <alsthdua107@gmail.com>
2026-07-21 15:38:25 +0900

    refactor: 산책 목록 조회 기능 삭제

--- src/main/java/com/daenggo/backend/walk/repository/WalkRecordRepository.java
+++ src/main/java/com/daenggo/backend/walk/repository/WalkRecordRepository.java
@@ -11,8 +11,8 @@ import com.daenggo.backend.walk.entity.WalkRecord;
 
 public interface WalkRecordRepository extends JpaRepository<WalkRecord, Long> {
 		
-	// 회원의 전체 산책 목록
-    List<WalkRecord> findByUserOrderByStartedAtDesc(User user);
+//	// 회원의 전체 산책 목록
+//    List<WalkRecord> findByUserOrderByStartedAtDesc(User user);
 
     // 회원의 특정 산책 조회
     Optional<WalkRecord> findByWalkRecordIdAndUser(Long walkRecordId, User user);
